5839e0a1bed159b7fe11dc68fb2626da939fc61b
Felix Buehler <account@buehler.rocks>
2019-11-04 10:58:11 +0100

    add ex2-practical

---
+++ assignment2/handout/src/main/java/de/unistgt/ipvs/vs/ex1/client/CalcSocketClient.java
@@ -0,0 +1,63 @@
+package de.unistgt.ipvs.vs.ex1.client;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+import java.net.UnknownHostException;
+
+/**
+ * Implement the connectTo-, disconnect-, and calculate-method of this class
+ * as necessary to complete the assignment. You may also add some fields or methods.
+ */
+public class CalcSocketClient {
+	private Socket cliSocket;
+	private int    rcvdOKs;		// --> Number of valid message contents
+	private int    rcvdErs;		// --> Number of invalid message contents
+	private int    calcRes;		// --> Calculation result (cf.  'RES')
+	
+	public CalcSocketClient() {
+		this.cliSocket = null;
+		this.rcvdOKs   = 0;
+		this.rcvdErs   = 0;
+		this.calcRes   = 0;
+	}
+	
+	public int getRcvdOKs() {
+		return rcvdOKs;
+	}
+
+	public int getRcvdErs() {
+		return rcvdErs;
+	}
+
+	public int getCalcRes() {
+		return calcRes;
+	}
+
+	public boolean connectTo(String srvIP, int srvPort) {
+               
+		//Solution here
+		
+		return true;
+	}
+
+	public boolean disconnect() {
+               
+	    //Solution here
+               
+		return true;
+	}
+
+	public boolean calculate(String request) {
+               
+		if (cliSocket == null) {
+			System.err.println("Client not connected!");
+			return false;
+		}
+		
+		//Solution here
+		
+		return true;
+	}
+}
