ebfbd5e71de264172896c50141f8931affac241d
Fabian Bühler <17296905+buehlefs@users.noreply.github.com>
2020-01-05 16:37:21 +0100

    Add implementation for c)

--- assignment5/A5Q5-Replication-Task/src/main/java/de/uni_stuttgart/ipvs/ids/replication/MajorityConsensus.java
+++ assignment5/A5Q5-Replication-Task/src/main/java/de/uni_stuttgart/ipvs/ids/replication/MajorityConsensus.java
@@ -1,7 +1,10 @@
 package de.uni_stuttgart.ipvs.ids.replication;
 
+import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutput;
 import java.io.ObjectOutputStream;
 import java.net.DatagramPacket;
 import java.net.DatagramSocket;
@@ -11,6 +14,10 @@ import java.net.SocketException;
 import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashSet;
+import java.util.Vector;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 import de.uni_stuttgart.ipvs.ids.communication.MessageWithSource;
 import de.uni_stuttgart.ipvs.ids.communication.NonBlockingReceiver;
@@ -45,17 +52,43 @@ public class MajorityConsensus<T> {
 	 * Part c) Implement this method.
 	 */
 	protected Collection<MessageWithSource<Vote>> requestReadVote() throws QuorumNotReachedException {
-		// TODO: Implement me!
-		return null;
+		RequestReadVote message = new RequestReadVote();
+		try {
+			byte[] byteMsg = this.serializeMessage(message);
+			for (SocketAddress replica: this.replicas) {
+				this.sendMessage(replica, byteMsg);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		int required = (int) (this.replicas.size() / 2) + 1; // TODO check quorum condition...
+		Vector<DatagramPacket> rawMessages = this.nbio.receiveMessages(5000, this.replicas.size()); // TODO adjust timeout
+		try {
+			Collection<MessageWithSource<Vote>> messages = NonBlockingReceiver.unpack(rawMessages);
+			return this.checkQuorum(messages);
+		} catch (IOException|ClassNotFoundException e) {
+			e.printStackTrace();
+		}
+		throw new QuorumNotReachedException(required, new HashSet<SocketAddress>());
 	}
-	
+
 	/**
 	 * Part c) Implement this method.
 	 */
 	protected void releaseReadLock(Collection<SocketAddress> lockedReplicas) {
-		// TODO: Implement me!
+		ReleaseReadLock message = new ReleaseReadLock();
+		try {
+			byte[] byteMsg = this.serializeMessage(message);
+			for (SocketAddress replica: lockedReplicas) {
+				this.sendMessage(replica, byteMsg);
+			}
+			// get replies out of the buffer
+			this.nbio.receiveMessages(5000, this.replicas.size());
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	
+
 	/**
 	 * Part d) Implement this method.
 	 */
@@ -63,36 +96,103 @@ public class MajorityConsensus<T> {
 		// TODO: Implement me!
 		return null;
 	}
-	
+
 	/**
 	 * Part d) Implement this method.
 	 */
 	protected void releaseWriteLock(Collection<SocketAddress> lockedReplicas) {
 		// TODO: Implement me!
 	}
-	
+
 	/**
 	 * Part c) Implement this method.
 	 */
 	protected T readReplica(SocketAddress replica) {
-		// TODO: Implement me!
-		return null;		
+		ReadRequestMessage readRequest = new ReadRequestMessage();
+
+		try {
+			this.sendMessage(replica, this.serializeMessage(readRequest));
+			return this.receiveValue();
+		} catch (IOException e) {
+			e.printStackTrace();
+			return null;
+		}
 	}
-	
+
 	/**
 	 * Part d) Implement this method.
 	 */
 	protected void writeReplicas(Collection<SocketAddress> lockedReplicas, VersionedValue<T> newValue) {
 		// TODO: Implement me!
 	}
-	
+
+	private byte[] serializeMessage(Object serializable) throws IOException {
+		try (ByteArrayOutputStream bStream = new ByteArrayOutputStream()) {
+			try (ObjectOutput oo = new ObjectOutputStream(bStream)) {
+				oo.writeObject(serializable);
+			}
+			return bStream.toByteArray();
+		}
+	}
+
+	private void sendMessage(SocketAddress replica, byte[] byteMsg) {
+		try {
+			this.socket.send(new DatagramPacket(byteMsg, byteMsg.length, replica));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	private <V> V deserializeMessage(DatagramPacket packet) throws IOException {
+		try (ByteArrayInputStream bStream = new ByteArrayInputStream(packet.getData())) {
+			try (ObjectInputStream iStream = new ObjectInputStream(bStream)) {
+				return (V) iStream.readObject();
+			} catch (ClassNotFoundException e) {
+				throw new IOException(e);
+			}
+		}
+	}
+
+	private T receiveValue() throws IOException {
+		byte[] buffer = new byte[2048];
+		DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+		socket.receive(packet);
+		try {
+			ValueResponseMessage<T> message = deserializeMessage(packet);
+			return message.getValue();
+		} catch (ClassCastException e) {
+			// TODO handle NO votes correctly
+			e.printStackTrace();
+			return null;
+		}
+	}
+
 	/**
 	 * Part c) Implement this method (and checkQuorum(), see below) to read the
 	 * replicated value using the majority consensus protocol.
 	 */
 	public VersionedValue<T> get() throws QuorumNotReachedException {
-		// TODO: Implement me!
-		return null;
+		Collection<SocketAddress> replicasToRelease = this.replicas;
+		try {
+			Collection<MessageWithSource<Vote>> lockedReplicas = this.requestReadVote();
+			replicasToRelease = lockedReplicas.stream().map(msg -> msg.getSource()).collect(Collectors.toList());
+			MessageWithSource<Vote> replicaToRead = lockedReplicas.stream()
+				.reduce(null, (resultA, resultB) -> {
+					if (resultA == null) {
+						return resultB;
+					}
+					if (resultB == null) {
+						return resultA;
+					}
+					if (resultA.getMessage().getVersion() >= resultB.getMessage().getVersion()) {
+						return resultA;
+					}
+					return resultB;
+				});
+			return new VersionedValue<T>(replicaToRead.getMessage().getVersion(), this.readReplica(replicaToRead.getSource()));
+		} finally {
+			this.releaseReadLock(replicasToRelease);
+		}
 	}
 
 	/**
@@ -108,12 +208,19 @@ public class MajorityConsensus<T> {
 	 * replies were received. If a sufficient number was received, this method
 	 * should return the {@link MessageWithSource}s of the locked {@link Replica}s.
 	 * Otherwise, a QuorumNotReachedException must be thrown.
-	 * @throws QuorumNotReachedException 
+	 * @throws QuorumNotReachedException
 	 */
 	protected Collection<MessageWithSource<Vote>> checkQuorum(
 			Collection<MessageWithSource<Vote>> replies) throws QuorumNotReachedException {
-		// TODO: Implement me!
-		return null;
+		Collection<MessageWithSource<Vote>> validReplies = replies.stream().filter((msg) -> {
+			return msg.getMessage().getState() == State.YES;
+		}).collect(Collectors.toList());
+		int required = (int) (this.replicas.size() / 2) + 1; // TODO check quorum condition...
+		if (validReplies.size() < required) {
+			Collection<SocketAddress> achieved = validReplies.stream().map(msg -> msg.getSource()).collect(Collectors.toList());
+			throw new QuorumNotReachedException(required, achieved);
+		}
+		return validReplies;
 	}
 
 }
